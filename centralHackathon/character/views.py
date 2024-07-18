5029afd535cd8dd3fdf8c1cc9a51bf1381a59213
sen <5.22nhz@gmail.com>
2024-07-18 17:12:22 +0900

    Fix: 타이머 경험치 중복 획득 문제 해결

--- centralHackathon/character/views.py
+++ centralHackathon/character/views.py
@@ -9,38 +9,29 @@ from datetime import datetime
 @login_required
 def home_page(request):
     character, created = Character.objects.get_or_create(user=request.user, defaults={'name': request.user.username})
-    # 한국 시간대 설정
+    
+    # 한국 표준 시간 설정
     KST = pytz.timezone('Asia/Seoul')
     now = datetime.now(KST)
 
-    # 자정에 초기화
-    if now.hour == 1 and now.minute == 3:
-        if request.method == 'POST':
-            data = json.loads(request.body)
-            elapsed_time = int(data.get('elapsed_time', 0))
-
-        # 경험치 계산 및 업데이트
-            experience_to_add = (elapsed_time // 10) * 200
+    # 자정 초기화
+    if now.hour == 1 and now.minute == 12:
+        if character.last_elapsed_time > 0:
+            experience_to_add = ((elapsed_time - character.last_elapsed_time) // 10) * 200
             character.add_experience(experience_to_add)
+            character.last_elapsed_time = 0
             character.save()
-
-            return JsonResponse({'experience': character.experience})  # Send experience back to update immediately
-        
-        character.last_elapsed_time = 0
-        character.save()
-
-        return render(request, 'homee.html', {'character': character})
 
     if request.method == 'POST':
         data = json.loads(request.body)
         elapsed_time = int(data.get('elapsed_time', 0))
 
         # 경험치 계산 및 업데이트
-        experience_to_add = (elapsed_time // 10) * 200
+        experience_to_add = ((elapsed_time - character.last_elapsed_time) // 10) * 200
         character.add_experience(experience_to_add)
         character.last_elapsed_time = elapsed_time  # 마지막 타이머 시간 저장
         character.save()
 
-        return JsonResponse({'experience': character.experience})  # Send experience back to update immediately
+        return JsonResponse({'experience': character.experience})
 
     return render(request, 'homee.html', {'character': character})
