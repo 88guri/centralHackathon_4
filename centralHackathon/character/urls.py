0f8266a826447f575cd4c00c963503a2fc5aa27f
sen <5.22nhz@gmail.com>
2024-07-21 19:33:23 +0900

    Feat: 총 집중 시간

--- centralHackathon/character/urls.py
+++ centralHackathon/character/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import timer_page, create_character
+from .views import timer_page, create_character, history_page
 
 urlpatterns = [
     path('timer/', timer_page, name='timer_page'),
     path('create_character/', create_character, name='create_character'),
+    path('history/', history_page, name='history_page'),
 ]
