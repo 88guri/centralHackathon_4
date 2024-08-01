fde7b6448b6f55f7d918fd62ee19ec229554b8dd
sen <5.22nhz@gmail.com>
2024-08-01 15:04:57 +0900

    Feat: 광고 2배 보상 랜덤 제공

--- centralHackathon/character/urls.py
+++ centralHackathon/character/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import timer_page, create_character, history_page, detailed_history, character_missing, watch_ad, deco, claim_reward, character_lost_forever
+from .views import timer_page, create_character, history_page, detailed_history, character_missing, watch_ad, deco, claim_reward, character_lost_forever, watch_ad_reward, watch_ad2
 
 urlpatterns = [
     path('timer/', timer_page, name='timer_page'),
@@ -11,4 +11,6 @@ urlpatterns = [
     path('deco/', deco, name='deco'),
     path('claim_reward/', claim_reward, name='claim_reward'),
     path('character_lost_forever/', character_lost_forever, name='character_lost_forever'),
+    path('watch_ad_reward/', watch_ad_reward, name='watch_ad_reward'),
+    path('watch_ad2/', watch_ad2, name='watch_ad2'),
 ]
